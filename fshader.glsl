24f2aaf7c6a13ef3c18e36f18d86a81189a06f69
Knut Arild Erstad <knutae@gmail.com>
2018-09-08 19:33:21 +0200

    First example, a raymarched sphere with ortographic projection.

--- fshader.glsl
+++ fshader.glsl
@@ -3,10 +3,33 @@ varying vec2 vTexCoord;
 uniform float uAspect;
 uniform float uTime;
 
+float origin_sphere(vec3 p, float radius) {
+    return length(p) - radius;
+}
+
+float scene(vec3 p) {
+    return origin_sphere(p, 0.5);
+}
+
+vec3 ray_march(vec3 start_pos, vec3 direction) {
+    vec3 p = start_pos;
+    float hit_color = 1.0;
+    for (int i = 0; i < 20; i++) {
+        float dist = scene(p);
+        if (dist < 0.01) {
+            return vec3(hit_color);
+        }
+        p += direction * dist;
+        hit_color -= 0.05;
+    }
+    return vec3(0.0);
+}
+
 void main() {
-    vec3 color = vec3(
-        sin(vTexCoord.x + uTime) / 2.0 + 0.5,
-        sin(vTexCoord.y + uTime + 1.0) / 2.0 + 0.5,
-        sin(-uTime) / 2.0 + 0.5);
+    float u = vTexCoord.x - 1.0;
+    float v = (vTexCoord.y - 1.0) / uAspect;
+    vec3 start_pos = vec3(u, v, -5.0);
+    vec3 direction = vec3(0.0, 0.0, 1.0);
+    vec3 color = ray_march(start_pos, direction);
     gl_FragColor = vec4(color, 1.0);
 }
--- presentation.md
+++ presentation.md
@@ -12,6 +12,39 @@ Prepared in advance. Explain a bit.
 
 Raymarched sphere with orthographic projection.
 
+```glsl
+float origin_sphere(vec3 p, float radius) {
+    return length(p) - radius;
+}
+
+float scene(vec3 p) {
+    return origin_sphere(p, 0.5);
+}
+
+vec3 ray_march(vec3 start_pos, vec3 direction) {
+    vec3 p = start_pos;
+    float hit_color = 1.0;
+    for (int i = 0; i < 20; i++) {
+        float dist = scene(p);
+        if (dist < 0.01) {
+            return vec3(hit_color);
+        }
+        p += direction * dist;
+        hit_color -= 0.05;
+    }
+    return vec3(0.0);
+}
+
+void main() {
+    float u = vTexCoord.x - 1.0;
+    float v = (vTexCoord.y - 1.0) / uAspect;
+    vec3 start_pos = vec3(u, v, -5.0);
+    vec3 direction = vec3(0.0, 0.0, 1.0);
+    vec3 color = ray_march(start_pos, direction);
+    gl_FragColor = vec4(color, 1.0);
+}
+```
+
 ## Multiple spheres, position offsets
 
 ## Perspective projection
